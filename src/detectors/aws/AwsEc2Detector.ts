2fa495d039f5627c6edd6923999e962033fc553a
Anupam Dalmia <anupam.dalmia@logicmonitor.com>
2022-01-10 22:32:45 +0530

    fix: linting changes

--- src/detectors/aws/AwsEc2Detector.ts
+++ src/detectors/aws/AwsEc2Detector.ts
@@ -1,32 +1,26 @@
 import {
-    Detector,
-    Resource,
-    ResourceDetectionConfig,
+	Detector,
+	Resource,
+	ResourceDetectionConfig,
 } from '@opentelemetry/resources';
 import { awsEc2Detector as otelAWSEc2Detector } from '@opentelemetry/resource-detector-aws';
 
-
 class AwsEc2Detector implements Detector {
-
-    async detect(_config?: ResourceDetectionConfig): Promise<Resource> {
-        
-        try {
-            
-            let awsResource = await otelAWSEc2Detector.detect();
-            let arn = `aws:ec2:${awsResource.attributes['cloud.region']}:${awsResource.attributes['cloud.account.id']}:instance/${awsResource.attributes['host.id']}`
-            const updatedResource = new Resource({
-                'aws.arn': arn
-            })
-            awsResource = awsResource.merge(updatedResource)
-            return awsResource;
-        }
-        //TODO: implement PrivateIP detection
-        catch(e) {
-            console.log("Error: ", e);
-            return Resource.empty();
-        }
-        
-    }
+	async detect(_config?: ResourceDetectionConfig): Promise<Resource> {
+		try {
+			let awsResource = await otelAWSEc2Detector.detect();
+			const arn = `aws:ec2:${awsResource.attributes['cloud.region']}:${awsResource.attributes['cloud.account.id']}:instance/${awsResource.attributes['host.id']}`;
+			const updatedResource = new Resource({
+				'aws.arn': arn,
+			});
+			awsResource = awsResource.merge(updatedResource);
+			return awsResource;
+		} catch (e) {
+			// TODO: implement PrivateIP detection
+			console.log('Error: ', e);
+			return Resource.empty();
+		}
+	}
 }
 
 export const awsEc2Detector = new AwsEc2Detector();
--- src/detectors/aws/AwsEcsDetector.ts
+++ src/detectors/aws/AwsEcsDetector.ts
@@ -1,28 +1,22 @@
 import {
-    Detector,
-    Resource,
-    ResourceDetectionConfig,
+	Detector,
+	Resource,
+	ResourceDetectionConfig,
 } from '@opentelemetry/resources';
 
 import { awsEcsDetector as otelAWSEcsDetector } from '@opentelemetry/resource-detector-aws';
 
 class AwsEcsDetector implements Detector {
+	async detect(_config?: ResourceDetectionConfig): Promise<Resource> {
+		try {
+			const awsResource = await otelAWSEcsDetector.detect();
 
-    async detect(_config?: ResourceDetectionConfig): Promise<Resource> {
-        
-        try {
-            
-            let awsResource = await otelAWSEcsDetector.detect();
-        
-            return awsResource;
-        }
-        
-        catch(e) {
-            console.log("Error: ", e);
-            return Resource.empty();
-        }
-        
-    }
+			return awsResource;
+		} catch (e) {
+			console.log('Error: ', e);
+			return Resource.empty();
+		}
+	}
 }
 
 export const awsEcsDetector = new AwsEcsDetector();

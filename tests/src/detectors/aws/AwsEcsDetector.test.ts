72baa4983633dbc85c6ec7503cd58ae211a55d9d
Anupam Dalmia <anupam.dalmia@logicmonitor.com>
2022-01-10 22:50:28 +0530

    fix: linting fixes in test cases

--- tests/src/detectors/aws/AwsEcsDetector.test.ts
+++ tests/src/detectors/aws/AwsEcsDetector.test.ts
@@ -1,51 +1,43 @@
 import { awsEcsDetector } from '../../../../src/detectors/aws/AwsEcsDetector';
 import { mocked } from 'ts-jest/utils';
 import { awsEcsDetector as otelAWSEcsDetector } from '@opentelemetry/resource-detector-aws';
-const { Resource } =require( "@opentelemetry/resources");
-//const { SemanticResourceAttributes } =require( "@opentelemetry/semantic-conventions");
+const { Resource } = require('@opentelemetry/resources');
+// const { SemanticResourceAttributes } =require( "@opentelemetry/semantic-conventions");
 
-jest.mock('@opentelemetry/resource-detector-aws')
+jest.mock('@opentelemetry/resource-detector-aws');
 
 beforeEach(() => {
-    mocked(otelAWSEcsDetector.detect).mockClear();
-})
+	mocked(otelAWSEcsDetector.detect).mockClear();
+});
 
 describe('AwsEcsDetector', () => {
-  
-    it('should be defined', () => {
-      expect(awsEcsDetector).toBeDefined();  
-    }); 
-    
-    it('should return empty Resource if ECS not detected', async () => {
-          
-        mocked(otelAWSEcsDetector.detect).mockImplementation((): Promise<any> => {
-            return Promise.resolve(Resource.empty())
-        
-        })
-    
-        const awsResource = await awsEcsDetector.detect();
-    
-        expect(mocked(otelAWSEcsDetector.detect).mock.calls.length).toBe(1);
-        expect(awsResource).toBeDefined();
-        expect(awsResource).toBe(Resource.empty());
-        expect(awsResource.attributes['aws.arn']).toBeUndefined();
-    
-  })
-  
-    it('should return empty Resource if there is an exception', async () => {
-          
-          mocked(otelAWSEcsDetector.detect).mockImplementation((): Promise<any> => {
-              return Promise.reject(new Error('error'))
-          
-          })
-      
-          const awsResource = await awsEcsDetector.detect();
-      
-          expect(mocked(otelAWSEcsDetector.detect).mock.calls.length).toBe(1);
-          expect(awsResource).toBeDefined();
-          expect(awsResource).toBe(Resource.empty());
-          expect(awsResource.attributes['aws.arn']).toBeUndefined();
-      
-    })
-  
-  });
+	it('should be defined', () => {
+		expect(awsEcsDetector).toBeDefined();
+	});
+
+	it('should return empty Resource if ECS not detected', async () => {
+		mocked(otelAWSEcsDetector.detect).mockImplementation((): Promise<any> => {
+			return Promise.resolve(Resource.empty());
+		});
+
+		const awsResource = await awsEcsDetector.detect();
+
+		expect(mocked(otelAWSEcsDetector.detect).mock.calls.length).toBe(1);
+		expect(awsResource).toBeDefined();
+		expect(awsResource).toBe(Resource.empty());
+		expect(awsResource.attributes['aws.arn']).toBeUndefined();
+	});
+
+	it('should return empty Resource if there is an exception', async () => {
+		mocked(otelAWSEcsDetector.detect).mockImplementation((): Promise<any> => {
+			return Promise.reject(new Error('error'));
+		});
+
+		const awsResource = await awsEcsDetector.detect();
+
+		expect(mocked(otelAWSEcsDetector.detect).mock.calls.length).toBe(1);
+		expect(awsResource).toBeDefined();
+		expect(awsResource).toBe(Resource.empty());
+		expect(awsResource.attributes['aws.arn']).toBeUndefined();
+	});
+});
--- tests/src/detectors/gcp/GcpDetectors.test.ts
+++ tests/src/detectors/gcp/GcpDetectors.test.ts
@@ -1,44 +1,42 @@
-import { gcpDetector } from "../../../../src/detectors/gcp/GcpDetector";
-import { mocked } from "ts-jest/utils";
+import { gcpDetector } from '../../../../src/detectors/gcp/GcpDetector';
+import { mocked } from 'ts-jest/utils';
 import { gcpDetector as otelGCPDetector } from '@opentelemetry/resource-detector-gcp';
-import { Resource } from "@opentelemetry/resources";
-const { SemanticResourceAttributes } =require( "@opentelemetry/semantic-conventions");
+import { Resource } from '@opentelemetry/resources';
+const {
+	SemanticResourceAttributes,
+} = require('@opentelemetry/semantic-conventions');
 
-jest.mock("@opentelemetry/resource-detector-gcp")
+jest.mock('@opentelemetry/resource-detector-gcp');
 
 beforeEach(() => {
-    mocked(otelGCPDetector.detect).mockClear();
-})
+	mocked(otelGCPDetector.detect).mockClear();
+});
 
-describe("GCP Detectors", () => {
-    it('should return empty resource if GCP not detected', async () => {
+describe('GCP Detectors', () => {
+	it('should return empty resource if GCP not detected', async () => {
+		mocked(otelGCPDetector.detect).mockImplementation((): Promise<any> => {
+			return Promise.reject(new Error('GCP not detected'));
+		});
 
-        mocked(otelGCPDetector.detect).mockImplementation((): Promise<any> => {
-            return Promise.reject({})
-        })
+		const resource = await gcpDetector.detect();
+		expect(resource).toEqual(Resource.empty());
+	});
 
-        const resource = await gcpDetector.detect();
-        expect(resource).toEqual(Resource.empty());
-    })
+	it('should return GCP resource if detected', async () => {
+		const expectedResource = new Resource({
+			[SemanticResourceAttributes.CLOUD_ACCOUNT_ID]:
+				'mycompany.com:api-project-123456789012',
+			[SemanticResourceAttributes.CLOUD_AVAILABILITY_ZONE]: 'asia-south1-c',
+			[SemanticResourceAttributes.HOST_ID]: '1234567890123456789',
+			[SemanticResourceAttributes.CLOUD_PROVIDER]: 'gcp',
+		});
 
-    it('should return GCP resource if detected', async () => {
+		mocked(otelGCPDetector.detect).mockImplementation((): Promise<any> => {
+			return Promise.resolve(expectedResource);
+		});
 
-        const expectedResource = new Resource({
-           
-                [SemanticResourceAttributes.CLOUD_ACCOUNT_ID]: "mycompany.com:api-project-123456789012",
-                [SemanticResourceAttributes.CLOUD_AVAILABILITY_ZONE]: "asia-south1-c",
-                [SemanticResourceAttributes.HOST_ID]: "1234567890123456789",
-                [SemanticResourceAttributes.CLOUD_PROVIDER]: "gcp"
-            
-        })
+		const resource = await gcpDetector.detect();
 
-        mocked(otelGCPDetector.detect).mockImplementation((): Promise<any> => {
-            return Promise.resolve(expectedResource)
-        })
-
-        const resource = await gcpDetector.detect()
-
-        expect(resource).toEqual(expectedResource)
-    })
-
-})
+		expect(resource).toEqual(expectedResource);
+	});
+});

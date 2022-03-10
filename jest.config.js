2722c3099dbc639ee52dbc31ccee092abf10b81e
Anupam Dalmia <anupamdalmia10@users.noreply.github.com>
2022-03-11 00:52:55 +0530

    fix: add jest config to ignore build directory

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.ts'],
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleDirectories: ['node_modules', 'src'],
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.ts'],
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	moduleDirectories: ['node_modules', 'src'],
+	modulePathIgnorePatterns: ['./build/'],
 };
